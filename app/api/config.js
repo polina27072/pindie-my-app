2bde6a3f054d09d0c3990688599d656a757317fe
polina27072 <fadpauls@yandex.ru>
2024-05-21 15:59:22 +0300

    http - https

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://polina-backend.nomoredomainswork.ru/api'
+export const BASE_URL = 'https://polina-backend.nomoredomainswork.ru/api'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
